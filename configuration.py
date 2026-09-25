3fad1e5d98c2aa6af7a15ba427e2e01d2107ba9d
Ksenia-SKr <timofei.neko@gmail.com>
2026-09-25 17:39:42 +0300

    Комментарии к sql_request и исправления в create_new_order_test

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://4acf8b7d-fc12-4f7a-80e7-cd8f8d9e93c3.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://6af2d861-3488-45f5-9371-f55ce7108e8d.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 ORDER_TRACK_PATH = "/api/v1/orders/track"
--- create_new_order_test.py
+++ create_new_order_test.py
@@ -1,13 +1,17 @@
 #Скрысанова Ксения, 47 когорта - финальный проект, инженере по тестированию плюс
+# импортируем нужные нам данные
 import sender_stand_request
 import data
 
 def test_get_order_by_track():
+    # создаем новый заказа
     new_order_response = sender_stand_request.post_new_order(data.order_body)
+    # убеждаемся, что заказ создан и на этом этапе ошибки нет
     assert new_order_response.status_code == 201
+    # получаем трек заказа, который должен быть в ответе и в формате json
     json_data = new_order_response.json()
     track_id = json_data["track"]
-    print ("Трек закза", track_id)
+    # получаем заказ через трек
     get_new_order = sender_stand_request.get_order(track_id)
+    # убеждаемся, что функция сработала верно, ожидаемы ответ системы 200
     assert get_new_order.status_code == 200
-    print (get_new_order.status_code)
